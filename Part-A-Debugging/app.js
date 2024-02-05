825d528e273c9813e4bacebfb3f3ac7d2b8a6ad5
Gathrean Dela Cruz <gathrean@icloud.com>
2024-02-04 21:52:59 -0800

    Finished Part A

---
+++ Part-A-Debugging/app.js
@@ -0,0 +1,5 @@
+const mo = require('./modules/math.js');
+
+console.log(`Hello Gathrean. The area is ${mo.area(5)}`);
+console.log(`5 + 3 = ${mo.add(5, 3)}`);
+console.log(`5 - 3 = ${mo.subtract(5, 3)}`);
---
+++ Part-A-Debugging/modules/math.js
@@ -0,0 +1,14 @@
+// DISCLOSURE: Used ChatGPT
+
+exports.add = function (num1, num2) {
+    return num1 + num2;
+}
+
+exports.subtract = function (num1, num2) {
+    return num1 - num2;
+}
+
+const PI = 3.14159;
+exports.area = function (radius) {
+    return PI * radius * radius;
+};
