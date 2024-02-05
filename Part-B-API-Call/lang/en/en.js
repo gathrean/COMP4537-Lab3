d46d3884f77fc1c7960b9e6e955cabf3352e7c34
Gathrean Dela Cruz <gathrean@icloud.com>
2024-02-04 23:40:47 -0800

    Update greeting function to handle missing name parameter

--- Part-B-API-Call/lang/en/en.js
+++ Part-B-API-Call/lang/en/en.js
@@ -1,4 +1,4 @@
 module.exports = {
-    greeting: name => `<p style="color: blue;">Hello, ${name}!`,
+    greeting: name => `<p style="color: blue;">Hello, ${name || 'Guest'}!`,
     serverTime: time => `The server time is ${time}.</p>`
 };
