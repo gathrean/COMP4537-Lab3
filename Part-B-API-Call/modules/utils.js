e57f3f0db781b877094e43ef64fb3075fa285343
Gathrean Dela Cruz <gathrean@icloud.com>
2024-02-04 23:40:06 -0800

    Changed timezone to los angeles

--- Part-B-API-Call/modules/utils.js
+++ Part-B-API-Call/modules/utils.js
@@ -1,6 +1,14 @@
 class Utils {
     static getCurrentTime() {
-        return new Date().toLocaleTimeString();
+        const options = {
+            timeZone: 'America/Los_Angeles',
+            hour12: true,
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        };
+
+        return new Date().toLocaleTimeString('en-US', options);
     }
 }
 
