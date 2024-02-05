599d8fbf1a7ecd62b5e8cb47c72e6b253c9dcbeb
Gathrean Dela Cruz <gathrean@icloud.com>
2024-02-04 23:52:03 -0800

    Update serverTime message to include timezone

--- Part-B-API-Call/lang/en/en.js
+++ Part-B-API-Call/lang/en/en.js
@@ -1,4 +1,4 @@
 module.exports = {
     greeting: name => `<p style="color: blue;">Hello, ${name || 'Guest'}!`,
-    serverTime: time => `The server time is ${time}.</p>`
+    serverTime: time => `The server time is ${time}. GMT-0800 (Pacific Standard Time)</p>`
 };
